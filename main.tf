b6211567ffc858bb2a8610e77c6c3218bff886f7
Jh4175 <jh4175@g.rit.edu>
2025-02-11 11:08:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ resource "aws_instance" "my_server" {
    instance_type = var.instance_type            # Use the instance type from variables
    key_name      = "${local.aws_key}"          # Specify the SSH key pair name
    user_data     = file("wp_install.sh")
-   security_groups = [aws_security_group.allow_http_ssh.name]
   
    # Add tags to the EC2 instance for identification
    tags = {
@@ -22,35 +21,4 @@ resource "aws_instance" "my_server" {
    }                  
 }
 
-resource "aws_security_group" "allow_http_ssh"{
-  name           = "allow_http"
-  description    = "Allow http inbound traffic"
 
-
-  ingress {
-    description = "http"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress{
-    description = "ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_http_ssh"
-  }
-}
